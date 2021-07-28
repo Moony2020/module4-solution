e638ed7944ada3057c12cad1e9d6bfe8140acdde
Moony2020 <74187134+Moony2020@users.noreply.github.com>
2021-07-28 04:15:14 +0200

    updated

--- mod4/Goodbye.js
+++ mod4/Goodbye.js
@@ -1,5 +1,5 @@
 (function (window) {
-    var byeSpeaker = {}; //Create an object, called 'helloSpeaker'
+    var byeSpeaker = {}; //Create an object, called 'helloSpeaker
     var speakWord = "Good Bye"; // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
     byeSpeaker.speak = function (name) { //  attach the "speak" method and expose to the global context
 
@@ -9,5 +9,3 @@
   //}
     window.byeSpeaker = byeSpeaker;    //Expose the 'helloSpeaker' object to the global scope
 })(window);
-
-
